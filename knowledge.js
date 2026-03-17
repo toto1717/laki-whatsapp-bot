aeb573d585d8fbeba38fb5b613ba877de6468873
toto1717 <antonio.kocovski@gmail.com>
2026-03-17 12:34:17 +0100

    knowledge.js

--- knowledge.js
+++ knowledge.js
@@ -59,7 +59,6 @@ const hotelKnowledge = {
         "контакт",
         "телефон",
         "мејл",
-        "email",
         "број",
       ],
       answerEn:
@@ -88,13 +87,53 @@ const hotelKnowledge = {
         "слободно",
         "резервација",
         "резервирај",
-        "соба",
-        "понуда",
       ],
       answerEn:
         "For prices and availability, please send us your stay details and we will prepare an offer.",
       answerMk:
         "За цени и достапност, испратете ни ги деталите за престојот и ќе ви подготвиме понуда.",
+      triggersInquiryFlow: true,
+    },
+
+    {
+      id: "rooms",
+      keywords: [
+        "room",
+        "rooms",
+        "accommodation",
+        "stay",
+        "apartment",
+        "suite",
+        "соба",
+        "соби",
+        "сместување",
+        "апартман",
+        "апартмани",
+      ],
+      answerEn:
+        "Laki Hotel & Spa offers comfortable accommodation options for couples, families and groups. Please contact us with your preferred dates, number of adults and children, and we will recommend the best room option for you.",
+      answerMk:
+        "Laki Hotel & Spa нуди удобни опции за сместување за парови, фамилии и групи. Испратете ни ги датумите, бројот на возрасни и деца, и ќе ви ја препорачаме најсоодветната соба.",
+    },
+
+    {
+      id: "offer",
+      keywords: [
+        "offer",
+        "offers",
+        "special offer",
+        "deal",
+        "package",
+        "accommodation offer",
+        "понуда",
+        "понуди",
+        "пакет",
+        "специјална понуда",
+      ],
+      answerEn:
+        "For the best accommodation offer, please send us your check-in date, check-out date, number of adults, number of children, your name and email, and we will prepare an offer for you.",
+      answerMk:
+        "За најдобра понуда за сместување, испратете ни check-in датум, check-out датум, број на возрасни, број на деца, име и e-mail, и ќе ви подготвиме понуда.",
       triggersInquiryFlow: true,
     },
 
@@ -106,15 +145,17 @@ const hotelKnowledge = {
         "where",
         "map",
         "locate",
-        "адреса",
+        "near",
         "локација",
+        "адреса",
         "каде",
         "мапа",
+        "блиску",
       ],
       answerEn:
-        "For the exact hotel location and directions, please contact us directly.",
+        "You can find our exact location here:\nhttps://www.google.com/maps/place/Hotel+%26+Spa+%E2%80%9ELaki%E2%80%9C/@41.068414,20.7991041,827m/data=!3m1!1e3!4m11!3m10!1s0x1350c37c9dd4da75:0xeb369b8ad996df4f!5m4!1s2026-04-04!2i4!4m1!1i2!8m2!3d41.068414!4d20.801679!16s%2Fg%2F11ddxpfd7j?entry=ttu&g_ep=EgoyMDI2MDMxMS4wIKXMDSoASAFQAw%3D%3D",
       answerMk:
-        "За точна локација и насоки до хотелот, ве молиме контактирајте не директно.",
+        "Локацијата на хотелот можете да ја видите тука:\nhttps://www.google.com/maps/place/Hotel+%26+Spa+%E2%80%9ELaki%E2%80%9C/@41.068414,20.7991041,827m/data=!3m1!1e3!4m11!3m10!1s0x1350c37c9dd4da75:0xeb369b8ad996df4f!5m4!1s2026-04-04!2i4!4m1!1i2!8m2!3d41.068414!4d20.801679!16s%2Fg%2F11ddxpfd7j?entry=ttu&g_ep=EgoyMDI2MDMxMS4wIKXMDSoASAFQAw%3D%3D",
     },
 
     {
@@ -246,9 +287,4 @@ function getFaqReply(message, language = "en") {
   };
 }
 
-module.exports = {
-  hotelKnowledge,
-  getFaqReply,
-  findBestFaqMatch,
-  normalizeText,
-};
+export { hotelKnowledge, getFaqReply, findBestFaqMatch, normalizeText };
