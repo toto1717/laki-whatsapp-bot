474eaf685ab155a3ea671400d693d2a642ad8431
toto1717 <antonio.kocovski@gmail.com>
2026-03-18 10:27:14 +0100

    mailer.js

--- mailer.js
+++ mailer.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.re_N4vp5Vfe_LyVWAVV5VZmpwj9QTktWYcoz);
 
 export async function sendInquiryEmail(inquiry) {
   const {
