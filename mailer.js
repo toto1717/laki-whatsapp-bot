a741c04fded55ef98b24d9ff15e0aeabd92060da
toto1717 <antonio.kocovski@gmail.com>
2026-03-18 09:49:51 +0100

    mailer.js

--- mailer.js
+++ mailer.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export async function sendInquiryEmail(inquiry) {
